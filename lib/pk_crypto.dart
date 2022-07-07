7ee3d0b091d3bdd139e690cf29015e13a3d80d5f
Ariel Tubaltsev <ariel@zxbase.com>
2022-07-07 12:21:05 -0700

    public key cryptography helpers
    Ed25519 helpers:
      - generation
      - serialization / deserialization
      - signing / verification

---
+++ lib/pk_crypto.dart
@@ -0,0 +1,93 @@
+// Copyright (C) 2022 Zxbase, LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/// Public key cryptography Ed25519 helpers:
+///   * Generation.
+///   * Serialization / deserialization.
+///   * Signing and verification.
+
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:cryptography/cryptography.dart';
+
+Ed25519 _algorithm = Ed25519();
+
+void _checkEd25519Jwk(Map<String, dynamic> json) {
+  if (!(json['crv'] == 'Ed25519' && json['kty'] == 'OKP')) {
+    throw FormatException('Key type is not supported $json');
+  }
+}
+
+String _padBase64(String str) {
+  if (str.length % 4 > 0) {
+    int pad = str.length % 4;
+    str += '=' * (4 - pad);
+  }
+  return str;
+}
+
+/// Generation.
+
+Future<SimpleKeyPair> generateKeyPair() async {
+  return await _algorithm.newKeyPair();
+}
+
+/// Serialization / deserialization.
+
+Map<String, dynamic> publicKeyToJwk(SimplePublicKey publicKey) {
+  // Ed25519 raw key is 32 bits.
+  return {
+    'kty': 'OKP',
+    'crv': 'Ed25519',
+    'x': base64Url.encode(publicKey.bytes)
+  };
+}
+
+SimplePublicKey jwkToPublicKey(Map<String, dynamic> json) {
+  _checkEd25519Jwk(json);
+  String strKey = _padBase64(json['x']);
+  Uint8List binaryKey = base64Url.decode(strKey);
+  return SimplePublicKey(binaryKey, type: KeyPairType.ed25519);
+}
+
+Future<Map<String, dynamic>> keyPairToJwk(SimpleKeyPair keyPair) async {
+  List<int> bytes = await keyPair.extractPrivateKeyBytes();
+  return {'kty': 'OKP', 'crv': 'Ed25519', 'x': base64Url.encode(bytes)};
+}
+
+Future<SimpleKeyPair> jwkToKeyPair(Map<String, dynamic> json) async {
+  _checkEd25519Jwk(json);
+  String strKey = _padBase64(json['x']);
+  Uint8List binaryKey = base64Url.decode(strKey);
+  return await _algorithm.newKeyPairFromSeed(binaryKey);
+}
+
+/// Signing and verification.
+
+Future<String> sign(String msg, SimpleKeyPair keyPair) async {
+  Signature sig = await _algorithm.sign(utf8.encode(msg), keyPair: keyPair);
+  return base64Url.encode(sig.bytes);
+}
+
+Future<bool> verifySignatureWithPublicKey(
+    String msg, String sig, SimplePublicKey publicKey) async {
+  Signature signature = Signature(base64Url.decode(sig), publicKey: publicKey);
+  return await _algorithm.verify(utf8.encode(msg), signature: signature);
+}
+
+Future<bool> verifySignature(
+    String msg, String sig, SimpleKeyPair keyPair) async {
+  SimplePublicKey pubKey = await keyPair.extractPublicKey();
+  return await verifySignatureWithPublicKey(msg, sig, pubKey);
+}
---
+++ test/pk_crypto_test.dart
@@ -0,0 +1,36 @@
+import 'package:zxbase_crypto/pk_crypto.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Generate 2 different Ed25519 keys', () async {
+    var kp1 = await generateKeyPair();
+    var kp2 = await generateKeyPair();
+
+    expect(kp1.extractPrivateKeyBytes(), isNot(kp2.extractPrivateKeyBytes()));
+    expect(kp1.extractPublicKey(), isNot(kp2.extractPublicKey()));
+  });
+
+  test('Sign and verify message', () async {
+    var kp = await generateKeyPair();
+    var msg = 'xxx';
+    var sig = await sign(msg, kp);
+    var rv = await verifySignature(msg, sig, kp);
+    expect(rv, equals(true));
+  });
+
+  test('Export and import key pair', () async {
+    // generate key, sign, export key, import, verify
+    var msg = 'xxx';
+    var kp = await generateKeyPair();
+    var sig = await sign(msg, kp);
+
+    var json = await keyPairToJwk(kp);
+    var importedKp = await jwkToKeyPair(json);
+    expect(await verifySignature(msg, sig, importedKp), equals(true));
+  });
+
+  test('Check jwk format', () async {
+    expect(
+        () => jwkToPublicKey({'kty': 'OKT'}), throwsA(isA<FormatException>()));
+  });
+}
